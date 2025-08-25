d4c13c3598a60776f84d34b460c6c9e9fe41359f
Kristina Balashova <infernalmajesty1350@gmail.com>
2025-08-25 17:47:27 +0200

    add: data card component

---
+++ src/components/DataCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  title: string;
+  value: number;
+  loading: boolean;
+}>();
+
+</script>
+
+<template>
+  <div
+    :class="[
+      'border border-gray-200 rounded-lg p-4 flex flex-col justify-between gap-2 bg-white shadow',
+      loading ? 'animate-pulse' : '',
+    ]"
+  >
+    <slot name="icon"></slot>
+    <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
+    <p v-if="!loading" class="text-gray-500">${{ value?.toLocaleString() }}</p>
+  </div>
+</template>
